c522a77f52dd2f4773f5e7737fb1b9a39705a073
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 11:58:30 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Guitar Tuner',
-  icon: 'mingcute:guitar-line',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Utilities'
 } satisfies ModuleConfig
